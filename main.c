212446c9bd36493198736fb0255f20ceffd5190d
Stulov Tikhon Sergeevich <stulov@author.spb.ru>
2021-06-28 21:35:12 +0300

    [msc] minor refactoring

--- main.c
+++ main.c
@@ -64,6 +64,7 @@
 #define BTN_PIN_MASK    \
     (GPIO_BTN_PIN_U | GPIO_BTN_PIN_C | GPIO_BTN_PIN_L | GPIO_BTN_PIN_D | GPIO_BTN_PIN_R)
 
+#define BTN_ACTIVE          (0)
 #define BTN_TOUT_DEB        (40)
 #define BTN_DBL_TOUT        (400)
 #define BTN_LONG_TOUT       (500)
@@ -72,15 +73,16 @@
 
 typedef enum
 {
-    BTN_NA,
+    BTN_NA = 0,
     BTN_PUSH,
     BTN_POP,
     BTN_CLICK,
     BTN_LONG_PRESS,
     BTN_REPEAT,
-    BTN_DBL_CLICK,
-    BTN_DBL_LONG_PRESS,
-    BTN_DBL_REPEAT,
+    BTN_DBL = 0x10,
+    BTN_DBL_CLICK      = BTN_CLICK      | BTN_DBL,
+    BTN_DBL_LONG_PRESS = BTN_LONG_PRESS | BTN_DBL,
+    BTN_DBL_REPEAT     = BTN_REPEAT     | BTN_DBL,
 } btn_event_t;
 
 typedef struct btn_s
@@ -89,7 +91,7 @@ typedef struct btn_s
     uint32_t       timer      ; // universal timer
     uint32_t       push_timer ; // interval between events BTN_PUSH
     btn_event_t    prev_status;
-    bool           dbl_flag   ;
+    btn_event_t    dbl        ;
     void           (*event)(struct btn_s *const _btn, const btn_event_t _event);
 } btn_t;
 
@@ -114,7 +116,11 @@ static __INLINE void _btn_event(btn_t *const _btn, const btn_event_t _event)
 
 void btn_debounce(btn_t *const _btn, const uint32_t _period, const uint16_t _port_val)
 {
+#if BTN_ACTIVE == 0
     const btn_event_t curr = (_port_val & _btn->pin) ? BTN_POP : BTN_PUSH;
+#else
+    const btn_event_t curr = (_port_val & _btn->pin) ? BTN_PUSH : BTN_POP;
+#endif
     
     // Debounce
     if (_btn->prev_status != curr)
@@ -138,7 +144,7 @@ void btn_debounce(btn_t *const _btn, const uint32_t _period, const uint16_t _por
         
         if (curr == BTN_PUSH)
         {
-            _btn->dbl_flag = (bool)(_btn->push_timer < BTN_DBL_TOUT);
+            _btn->dbl = (_btn->push_timer < BTN_DBL_TOUT) ? BTN_DBL : BTN_NA;
             _btn->push_timer = 0;
         }
     }
@@ -149,8 +155,7 @@ void btn_debounce(btn_t *const _btn, const uint32_t _period, const uint16_t _por
         && _btn->timer < BTN_DBL_TOUT
         )
     {
-        const btn_event_t event = _btn->dbl_flag ? BTN_DBL_CLICK : BTN_CLICK;
-        _btn_event(_btn, event); // BTN_DBL_CLICK, BTN_CLICK
+       _btn_event(_btn, BTN_CLICK | _btn->dbl); // BTN_DBL_CLICK, BTN_CLICK
     }
 
     if (_btn->prev_status == BTN_PUSH)
@@ -160,14 +165,12 @@ void btn_debounce(btn_t *const _btn, const uint32_t _period, const uint16_t _por
             && _btn->timer >= BTN_LONG_TOUT
             )
         {
-            const btn_event_t event = _btn->dbl_flag ? BTN_DBL_LONG_PRESS : BTN_LONG_PRESS;
-            _btn_event(_btn, event); // BTN_DBL_LONG_PRESS, BTN_LONG_PRESS
+            _btn_event(_btn, BTN_LONG_PRESS | _btn->dbl); // BTN_DBL_LONG_PRESS, BTN_LONG_PRESS
         }
         
         if (_btn->timer > BTN_REPEAT_TOUT)
         {
-            const btn_event_t event = _btn->dbl_flag ? BTN_DBL_REPEAT : BTN_REPEAT;
-            _btn_event(_btn, event); // BTN_DBL_REPEAT, BTN_REPEAT
+            _btn_event(_btn, BTN_REPEAT | _btn->dbl); // BTN_DBL_REPEAT, BTN_REPEAT
             _btn->timer -= BTN_REPEAT_INTERVAL;
         }
     }
